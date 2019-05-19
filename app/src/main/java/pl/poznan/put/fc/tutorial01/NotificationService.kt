4ed59152a6db33a99ff2f283805d85bd4d1b5a6c
writ3it <42292507+writ3it@users.noreply.github.com>
2019-05-19 16:42:49 +0200

    pobranie dźwięku systemowego

--- app/src/main/java/pl/poznan/put/fc/tutorial01/NotificationService.kt
+++ app/src/main/java/pl/poznan/put/fc/tutorial01/NotificationService.kt
@@ -1,12 +1,31 @@
 package pl.poznan.put.fc.tutorial01
 
+import android.media.RingtoneManager
+import android.net.Uri
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class NotificationService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage?) {
-        // TODO: miejsce na wygenerowanie powiadomienia
+        val rMessage = message!!
+        Log.i(TAG, "Message from: ${rMessage.from}")
+        createNotification(message)
         super.onMessageReceived(message)
+    }
+
+    private fun createNotification(message: RemoteMessage) {
+        val sound: Uri = getDefaultNotificationSound()
+        // TODO: budowanie powiadomienia
+        // TODO: uruchomienie menadżera powiadomień
+    }
+
+    private fun getDefaultNotificationSound(): Uri {
+        return RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+    }
+
+    companion object{
+        const val TAG:String = "NotificationService"
     }
 }
