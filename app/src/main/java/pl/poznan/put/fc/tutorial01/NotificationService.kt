2c6502a02a98d893fad37a9962c9579fe3bb01af
writ3it <42292507+writ3it@users.noreply.github.com>
2019-05-19 16:52:16 +0200

    konfiguracja buildera powiadomień

--- app/src/main/java/pl/poznan/put/fc/tutorial01/NotificationService.kt
+++ app/src/main/java/pl/poznan/put/fc/tutorial01/NotificationService.kt
@@ -2,6 +2,7 @@ package pl.poznan.put.fc.tutorial01
 
 import android.media.RingtoneManager
 import android.net.Uri
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -16,9 +17,19 @@ class NotificationService : FirebaseMessagingService() {
     }
 
     private fun createNotification(message: RemoteMessage) {
-        val sound: Uri = getDefaultNotificationSound()
-        // TODO: budowanie powiadomienia
+        val sound = getDefaultNotificationSound()
+        val builder = getNotificationBuilder(message,sound)
         // TODO: uruchomienie menadżera powiadomień
+    }
+
+    private fun getNotificationBuilder(message: RemoteMessage, sound: Uri):NotificationCompat.Builder{
+        val notification = message.notification!!
+        return NotificationCompat.Builder(this, getString(R.string.ChannelId))
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setSound(sound)
+        // TODO: aby dodać akcję dodaj wywołanie metody .setContentIntent(<PendingIntent>)
     }
 
     private fun getDefaultNotificationSound(): Uri {
